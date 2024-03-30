7cc792737c545258cf6b672e2415dce9b0e3d497
Jonathan Quinté <86927718+hardnessandextreme@users.noreply.github.com>
2024-03-30 11:04:52 -0500

    añadido label que muestra la fecha actual y funcionalidad temporal al boton guardar

--- src/components.py
+++ src/components.py
@@ -1,7 +1,9 @@
+import time
 from tkinter import Tk, Label, Button, messagebox, ttk, Entry
-from funcs import guardar_datos, convertir_excel, verificar_archivos, cargar_tipos_marcas
+from src.funcs import guardar_datos, convertir_excel, verificar_archivos, cargar_tipos_marcas
 
-def inicializar_componentes() -> None:
+
+def inicializar_componentes() -> Tk:
     tipos, marcas = cargar_tipos_marcas()
     
     ventana = Tk()
@@ -38,7 +40,29 @@ def inicializar_componentes() -> None:
     label_estado = Label(ventana, text="")
     label_estado.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
     
-    boton_guardar = Button(ventana, text="Guardar")
+    fecha_today = time.strftime("%d-%m-%Y")
+    label_fecha = Label(ventana, text=fecha_today)
+    label_fecha.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
+    
+    def guardar_datos_click(fecha: str, tipo: str, marca: str, modelo: str, num_serie: str, num_activo: str) -> None:
+        fecha = label_fecha.cget(key="text")
+        tipo = combo_tipo.get()
+        marca = combo_marca.get()
+        modelo = entry_modelo.get().upper()
+        num_serie = entry_num_serie.get().upper()
+        num_activo = "8042000 " + entry_num_activo.get().upper()
+
+        if modelo == "" or num_serie == "" or num_activo == "":
+            label_estado.config(text="Todos los campos son obligatorios")
+            return
+
+        if guardar_datos(fecha, tipo, marca, modelo, num_serie, num_activo):
+            label_estado.config(text="Datos guardados correctamente")
+        else:
+            label_estado.config(text="Error al guardar los datos")
+    
+    
+    boton_guardar = Button(ventana, text="Guardar", command=guardar_datos_click)
     boton_guardar.grid(row=5, column=0, padx=10, pady=10)
 
     boton_convertir_excel = Button(ventana, text="Convertir Excel")
